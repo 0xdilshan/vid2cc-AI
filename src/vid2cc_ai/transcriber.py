1ee68ca7f5e50fc64b449eb81eae502748580753
0xdilshan <257371012+0xdilshan@users.noreply.github.com>
2026-02-03 20:43:00 +0200

    Implemented OpenAI whisper based transcriber module

---
+++ src/vid2cc_ai/transcriber.py
@@ -0,0 +1,10 @@
+import whisper
+import torch
+
+class Transcriber:
+    def __init__(self, model_size="base"):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = whisper.load_model(model_size, device=self.device)
+
+    def transcribe(self, audio_path: str):
+        return self.model.transcribe(audio_path)['segments']
